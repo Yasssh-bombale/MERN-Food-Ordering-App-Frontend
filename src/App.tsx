73eca1b4fa59b603cb2a785e3e7feb7a99f0bd73
Yash Vijay Bombale <yashbombale@gmail.com>
2024-03-03 17:44:42 +0530

    initialize react app and shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return <Button>click me</Button>;
+}
+
+export default App;
