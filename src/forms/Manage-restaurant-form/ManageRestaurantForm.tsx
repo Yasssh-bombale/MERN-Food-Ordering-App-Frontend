88d233e23619bdb491506dfdf7706568a8c6b8a8
Yash Vijay Bombale <yashbombale@gmail.com>
2024-03-22 06:39:33 +0530

    Created formSchema and set zod resolver in restaurantForm

---
+++ src/forms/Manage-restaurant-form/ManageRestaurantForm.tsx
@@ -0,0 +1,56 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  restaurantName: z.string({
+    required_error: "restaurant name is required",
+  }),
+  city: z.string({
+    required_error: "restaurant name is required",
+  }),
+  country: z.string({
+    required_error: "restaurant name is required",
+  }),
+  deliveryPrice: z.coerce.number({
+    required_error: "delivery price is required",
+    invalid_type_error: "must be a valid number",
+  }),
+  estimatedDeliveryTime: z.coerce.number({
+    required_error: "estimated delivery time is required",
+    invalid_type_error: "must be a number",
+  }),
+  cuisines: z.array(z.string()).nonempty({
+    message: "please select at least one item",
+  }),
+  menuItems: z.array(
+    z.object({
+      name: z.string({
+        required_error: "name is required",
+      }),
+      price: z.coerce.number().min(1, "price is required"),
+    })
+  ),
+  imageFile: z.instanceof(File, { message: "Image is required" }),
+});
+
+type restaurantFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  onSave: (restaurantFormData: FormData) => void;
+  isLoading: boolean;
+};
+
+const ManageRestaurantForm = ({}: Props) => {
+  const form = useForm<restaurantFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cuisines: [],
+      menuItems: [{ name: "", price: 0 }],
+    },
+  });
+
+  return <div>ManageRestaurantForm</div>;
+};
+
+export default ManageRestaurantForm;
